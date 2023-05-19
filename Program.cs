04cd843703ca14d5d1649d8f7a6ff1257ccf4abe
Pang <narumonokanoto@gmail.com>
2023-05-19 19:57:12 +0700

    R1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("Input Number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        for(int row = 1; row <= number; row++) {
+            for(int col = 1; col <= row; col++) {
+                Console.Write("{0} ", col);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
